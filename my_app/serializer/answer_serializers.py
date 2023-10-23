7f4878df16a57f575ab963c8d9e7e8eb742df9d1
aLiyevTamerlan <tamerlan-aliyev-2021@mail.ru>
2023-10-23 10:40:03 +0400

    delete folders

--- my_app/serializer/answer_serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from my_app.models import Answer
-
-class AnswerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Answer
-        fields = ('id','answer')
