40c559d7b5890cbe1dbd3b188bd046d0f993df9b
aLiyevTamerlan <tamerlan-aliyev-2021@mail.ru>
2023-11-01 02:25:44 +0400

    add staticfiles

--- myproject/settings.py
+++ myproject/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'staticfiles'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
