7b6eb8f931dc9d92cfa156674d49450f1822b698
aLiyevTamerlan <tamerlan-aliyev-2021@mail.ru>
2024-01-27 01:58:06 +0400

    change logic of get random questions

--- my_app/views/questions_views.py
+++ my_app/views/questions_views.py
@@ -1,6 +1,7 @@
 import random, json
 from typing import List, Dict
 from django.db.models import Prefetch, Q
+from django.db import connection
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -50,14 +51,18 @@ class GetQuestionAPIView(APIView):
                     ser = QuestionSerializer(questions, many=True)
                     return Response(ser.data, status=status.HTTP_200_OK)
                 return Response([], status=status.HTTP_404_NOT_FOUND)
-        total_records = Question.objects.count()
-        offset = random.randint(0, max(0, total_records - 50))
-        random_questions = Question.objects.all()[offset:offset+50]
-        serializer = QuestionSerializer(random_questions, many=True)
-
+        random_instance_or_none = Question.objects.raw('''
+            SELECT * FROM {0}
+            WHERE id >= (SELECT FLOOR(RAND() * (SELECT MAX(id) FROM {0})))
+            ORDER BY RAND() LIMIT 50
+    
+        '''.format(Question._meta.db_table))
+        serializer = QuestionSerializer(random_instance_or_none, many=True)
         return Response(serializer.data)
 
-
+        # total_records = Question.objects.count()
+        # offset = random.randint(0, max(0, total_records - 50))
+        # random_questions = Question.objects.all()[offset:offset+50]
 class CheckExamAPIView(APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request):
@@ -119,28 +124,28 @@ class CheckCategoryQuestionAPIView(APIView): #change this api
         response_data = check_by_category(request_list=request_list, question_data=question_data, user=user)
         return Response(response_data, status=status.HTTP_200_OK)
 
-class GetQuestionByCategory(APIView):
-    permission_classes = [IsAuthenticated]
-    def get(self, request, category_name:str):
-        user = request.user
-        if category_name == 'Не решал':
-            category_questions = Statistic.objects.select_related("question_id").prefetch_related('question_id__answers').filter(user_id=user).values_list("question_id")
-            if category_questions.exists():
-                questions = Question.objects.prefetch_related('answers').exclude(id__in=category_questions)
-                ser = QuestionSerializer(questions, many=True)
-                return Response(ser.data, status=status.HTTP_200_OK)
-            else:
-                questions = Question.objects.prefetch_related('answers').all()
-                ser = QuestionSerializer(questions, many=True)
-                return Response(ser.data, status=status.HTTP_200_OK)
-        else:
-            category_questions = Statistic.objects.select_related("question_id").prefetch_related('question_id__answers').filter(Q(user_id=user) & Q(category=category_name)).values_list("question_id")
+# class GetQuestionByCategory(APIView):
+#     permission_classes = [IsAuthenticated]
+#     def get(self, request, category_name:str):
+#         user = request.user
+#         if category_name == 'Не решал':
+#             category_questions = Statistic.objects.select_related("question_id").prefetch_related('question_id__answers').filter(user_id=user).values_list("question_id")
+#             if category_questions.exists():
+#                 questions = Question.objects.prefetch_related('answers').exclude(id__in=category_questions)
+#                 ser = QuestionSerializer(questions, many=True)
+#                 return Response(ser.data, status=status.HTTP_200_OK)
+#             else:
+#                 questions = Question.objects.prefetch_related('answers').all()
+#                 ser = QuestionSerializer(questions, many=True)
+#                 return Response(ser.data, status=status.HTTP_200_OK)
+#         else:
+#             category_questions = Statistic.objects.select_related("question_id").prefetch_related('question_id__answers').filter(Q(user_id=user) & Q(category=category_name)).values_list("question_id")
 
-            if category_questions.exists():
-                questions = Question.objects.prefetch_related('answers').filter(id__in=category_questions)
-                ser = QuestionSerializer(questions, many=True)
-                return Response(ser.data, status=status.HTTP_200_OK)
-            return Response([], status=status.HTTP_404_NOT_FOUND)
+#             if category_questions.exists():
+#                 questions = Question.objects.prefetch_related('answers').filter(id__in=category_questions)
+#                 ser = QuestionSerializer(questions, many=True)
+#                 return Response(ser.data, status=status.HTTP_200_OK)
+#             return Response([], status=status.HTTP_404_NOT_FOUND)
 
 
 
