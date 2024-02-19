5c946b85319ca020336cec8d410228a9754638c9
aLiyevTamerlan <tamerlan-aliyev-2021@mail.ru>
2024-02-19 13:10:42 +0400

    add company filter by name

--- admin_app/views/company_views.py
+++ admin_app/views/company_views.py
@@ -1,3 +1,7 @@
+from django.db.models import Q
+from drf_yasg.utils import swagger_auto_schema 
+from drf_yasg import openapi
+
 from rest_framework import generics
 from rest_framework.response import Response
 from admin_app.serializers.company_serializers import CompanySerializer
@@ -10,13 +14,31 @@ class CompanyListCreateView(generics.ListCreateAPIView):
     serializer_class = CompanySerializer
     permission_classes = [IsAdminOrSuperUser]
     pagination_class = CompanyPagination
-
+    @swagger_auto_schema(
+            manual_parameters=[
+                
+                openapi.Parameter(
+                    'search',
+                    in_=openapi.IN_QUERY,
+                    type=openapi.TYPE_STRING,
+                )
+            ],responses={200: CompanySerializer},
+    )
     def get(self, request, *args, **kwargs):
         if 'page' not in request.query_params:
             # Disable pagination if 'page' is not present
             self.pagination_class = None
         return self.list(request, *args, **kwargs)
-    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.query_params.get('search', None)
+        filters = Q()
+        
+        if search_query:
+            filters |= Q(company_name__icontains=search_query)
+
+        queryset = queryset.filter(filters)
+        return queryset
     
 class CompanyRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Company.objects.all()
